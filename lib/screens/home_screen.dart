da7fe7dd70ce39e50a08fe860781ab6d0a12a7f3
Ruvini Karunaratna <ruvini.Karunaratna@gmail.com>
2023-03-17 17:34:35 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:market/core/controllers/sampleController.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(context) {
+    // Instantiate your class using Get.put() to make it available for all "child" routes there.
+    final SampleController c = Get.put(SampleController());
+
+    return Scaffold(
+        // Use Obx(()=> to update Text() whenever count is changed.
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+
+        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+        body: Center(
+            child: ElevatedButton(
+                child: const Text("Go to Other"),
+                onPressed: () => Get.toNamed("/products"))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
+  final SampleController c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
+  }
+}
