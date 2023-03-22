669ac82ac8ca907079e408eafe65d17383c6db55
Ruvini Karunaratna <ruvini.Karunaratna@gmail.com>
2023-03-22 23:26:07 +0530

    admin nav bar

---
+++ lib/core/widgets/NavDrawer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/cover.jpg'))),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () => {Get.toNamed("admin")},
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('Products'),
+            onTap: () => {Get.toNamed("/products")},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
