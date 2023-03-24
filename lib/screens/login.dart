f28a40914883ebb96321a36af89e9c080232757f
Dayan Marasinghe <dayanmarasinghe@gmail.com>
2023-03-24 20:56:38 +0530

    login image fixed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -43,68 +43,135 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Login'),
+    //   ),
+    //   body: _isLoading
+    //       ? Center(child: CircularProgressIndicator())
+    //       : Center(
+    //           child: Padding(
+    //             padding: const EdgeInsets.all(16.0),
+    //             child: Form(
+    //               key: _formKey,
+    //               child: Column(
+    //                 mainAxisAlignment: MainAxisAlignment.center,
+    //                 crossAxisAlignment: CrossAxisAlignment.center,
+    //                 children: <Widget>[
+    //                   Image.asset(
+    //                     'assets/images/login.png',
+    //                     height: 200,
+    //                   ),
+    //                   SizedBox(height: 20),
+    //                   TextFormField(
+    //                     controller: _emailController,
+    //                     decoration: InputDecoration(labelText: 'Email'),
+    //                     validator: (value) {
+    //                       if (value!.isEmpty) {
+    //                         return 'Please enter your email';
+    //                       }
+    //                       return null;
+    //                     },
+    //                   ),
+    //                   TextFormField(
+    //                     controller: _passwordController,
+    //                     decoration: InputDecoration(labelText: 'Password'),
+    //                     obscureText: true,
+    //                     validator: (value) {
+    //                       if (value!.isEmpty) {
+    //                         return 'Please enter your password';
+    //                       }
+    //                       return null;
+    //                     },
+    //                   ),
+    //                   SizedBox(height: 20),
+    //                   ElevatedButton(
+    //                     onPressed: () async {
+    //                       if (_formKey.currentState!.validate()) {
+    //                         await _loginUser();
+    //                       }
+    //                     },
+    //                     child: Text('Login'),
+    //                   ),
+    //                   SizedBox(height: 20),
+    //                   TextButton(
+    //                     onPressed: () {
+    //                       Navigator.of(context).pushNamed('/newuser');
+    //                     },
+    //                     child: Text('Don\'t have an account? Sign up.'),
+    //                   ),
+    //                 ],
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    // );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset(
-                        'assets/images/login.png',
-                        height: 200,
-                      ),
-                      SizedBox(height: 20),
-                      TextFormField(
-                        controller: _emailController,
-                        decoration: InputDecoration(labelText: 'Email'),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          return null;
-                        },
-                      ),
-                      TextFormField(
-                        controller: _passwordController,
-                        decoration: InputDecoration(labelText: 'Password'),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            await _loginUser();
-                          }
-                        },
-                        child: Text('Login'),
-                      ),
-                      SizedBox(height: 20),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/newuser');
-                        },
-                        child: Text('Don\'t have an account? Sign up.'),
-                      ),
-                    ],
-                  ),
+  appBar: AppBar(
+    title: Text('Login'),
+  ),
+  body: _isLoading
+      ? Center(child: CircularProgressIndicator())
+      : SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'assets/images/login.png',
+                      height: 200,
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(labelText: 'Email'),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await _loginUser();
+                        }
+                      },
+                      child: Text('Login'),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed('/newuser');
+                      },
+                      child: Text('Don\'t have an account? Sign up.'),
+                    ),
+                  ],
                 ),
               ),
             ),
-    );
+          ),
+        ),
+);
+
   }
 }
